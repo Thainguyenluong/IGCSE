90dfb5c687d89eda84844cfcb268595acccba4fb
ASUS <nlquocthai@gmail.com>
2018-01-25 15:03:15 +0700

    Array lesson

--- Git.py
+++ Git.py
@@ -1,3 +1,20 @@
-lunches=["potato thing","noodles"]
-days=["Monday","Tuedays"]
-print(lunches[1])
+lunches=["potato ","noodles","chicken","beef","soup"]
+days=["Monday","Tuedays","Wedndesday","Thursday","Friday"]
+
+#today=input("Put  a day:")
+#for i in range(0,5):
+    #if days[i]==today:
+        #print(lunches[i])
+
+day=input("Put a day:")
+
+if days.find(day)== False:
+    print("Day does not exist in my list")
+
+
+
+
+
+
+
+
