5f69724f80439089f3cd22299107bfdd09091140
ASUS <nlquocthai@gmail.com>
2018-02-01 15:03:54 +0700

    H

---
+++ Python project/1601.py
@@ -0,0 +1,12 @@
+name = input("Please enter your name:")
+name1 = input("Please enter your name:")
+name=name.lower()
+name1=name1.lower()
+
+if name == "Thai" :
+    print("Hello",name)
+
+elif name1=="mr waldron":
+        print("Hello sir")
+else:
+     print('Hello')
